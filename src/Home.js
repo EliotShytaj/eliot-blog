2d8881d8d1ce157919459662e6e7914295eb1c8b
Eliot Shytaj <eliotshytaj05@gmail.com>
2024-07-23 22:10:43 -0400

    adding blogs (list)

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
-
-    const handleClick = (e) => {
-        console.log("Hello, Hawks!", e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log("Hello," + name, e.target);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
 
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain("Lighty", e)}>Click me again</button>
+           {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                </div>   
+           ))} 
         </div>
      );
 }
